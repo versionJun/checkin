bcf86b6fb128388ea1b91616248f58c4b9dd150b
versionJun <55729868+versionJun@users.noreply.github.com>
2023-10-07 18:13:07 +0800

    Update v2free_checkin.js

--- task/v2free_checkin.js
+++ task/v2free_checkin.js
@@ -9,7 +9,8 @@ dayjs.extend(utc)
 dayjs.extend(timezone)
 dayjs.tz.setDefault('Asia/Shanghai')
 
-async function getV2freeCookie() {
+
+function getV2freeCookie() {
 
     let V2FREE_COOKIE = process.env.V2FREE_COOKIE || ''
     
@@ -28,6 +29,21 @@ async function getV2freeCookie() {
     return V2FREE_COOKIE_ARR
 }
 
+function getCookieMap(cookie){
+
+    const cookieMap = new Map();
+
+    decodeURIComponent(cookie)
+        .split(/\s*;\s*/)
+        .filter(item => item != '')
+        .forEach((item) => {
+            const [key, value] = item.split("=");
+            cookieMap.set(key, value);
+        })
+
+    return cookieMap
+}
+
 function go_checkin_url(cookie) {
     return axios('https://cdn.v2free.net/user/checkin', {
         method: 'POST',
@@ -37,6 +53,26 @@ function go_checkin_url(cookie) {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
             'Cookie': cookie
         }
+    })
+    .then(d => {
+        // console.log(checkin_result.data)
+        // { ret: 0, msg: '您似乎已经签到过了...' }
+        // {
+        //     msg: '获得了 313MB 流量.',
+        //     unflowtraffic: 1401946112,
+        //     traffic: '1.31GB',
+        //     trafficInfo: {
+        //         todayUsedTraffic: '0B',
+        //         lastUsedTraffic: '0B',
+        //         unUsedTraffic: '1.31GB'
+        //     },
+        //     ret: 1
+        // }
+
+        if (d.data.msg == undefined) 
+            return Promise.reject('cookie已过期或无效')
+
+        return d.data
     })
 }
 
@@ -48,54 +84,43 @@ function go_user_url(cookie){
             'Cookie': cookie
         }
     })
+    .then(d => {
+
+        const html_dom = new jsdom.JSDOM(d.data)
+
+        const unUsedTraffic = html_dom.window.document.querySelector('.nodename > a[href^="/user/trafficlog"]').textContent.trim()
+
+        return { unUsedTraffic }
+    })
 }
 
 !(async () => {
 
-    const V2FREE_COOKIE_ARR = await getV2freeCookie()
-
-    const COOKIE_INVALIDDITY = 'cookie已过期或无效'
+    const V2FREE_COOKIE_ARR = getV2freeCookie()
 
     let index = 1
     const message = []
-    for await (V2FREE_COOKIE of V2FREE_COOKIE_ARR) {
-        let remarks = "账号" + index
+    for (V2FREE_COOKIE of V2FREE_COOKIE_ARR) {
+        let account = `账号${index}`
+        let remarks = `${account}`
         try {
-            const cookieMap = new Map();
+            const cookieMap = getCookieMap(V2FREE_COOKIE);
 
-            decodeURIComponent(V2FREE_COOKIE)
-                .split(/\s*;\s*/)
-                .filter(item => item != '')
-                .forEach((cookie) => {
-                    const [key, value] = cookie.split("=");
-                    cookieMap.set(key, value);
-                })
+            const checkin_result = await go_checkin_url(V2FREE_COOKIE)
 
-            let checkin_result = await go_checkin_url(V2FREE_COOKIE)
+            remarks += `---${checkin_result.msg}`
 
-            let checkin_result_msg = (checkin_result.data.msg != undefined ? checkin_result.data.msg : COOKIE_INVALIDDITY)
+            remarks += `---${cookieMap.get("email")}`
 
-            remarks += `---${checkin_result_msg}`
+            if (checkin_result.ret == 1){
 
-            if (checkin_result_msg != COOKIE_INVALIDDITY) {
+                remarks += `---剩余流量:${checkin_result.trafficInfo.unUsedTraffic}`
 
-                remarks += `---${cookieMap.get("email")}`
-    
-                if (checkin_result.data.ret == 1){
-    
-                    remarks += `---剩余流量:${checkin_result.data.trafficInfo.unUsedTraffic}`
-    
-                } else {
-    
-                    let user_result = await go_user_url(V2FREE_COOKIE)
-    
-                    let html_dom = new jsdom.JSDOM(user_result.data)
-    
-                    let unUsedTraffic = html_dom.window.document.querySelector('.nodename > a[href^="/user/trafficlog"]').textContent.trim()
-    
-                    remarks += `---剩余流量:${unUsedTraffic}`
-    
-                }
+            } else {
+
+                const { unUsedTraffic } = await go_user_url(V2FREE_COOKIE)
+
+                remarks += `---剩余流量:${unUsedTraffic}`
 
             }
 
@@ -104,11 +129,14 @@ function go_user_url(cookie){
             message.push(remarks)
 
         } catch (e) {
+            console.log(`${account} catch > e = ${e}`);
             console.error(e)
-            message.push(remarks + "---" + e)
+            message.push(remarks + "---" +e)
         }
         index++
     }
+
+    // console.log(message)
 
     await sent_message_by_pushplus({ 
         title: `${path.parse(__filename).name}_${dayjs.tz().format('YYYY-MM-DD HH:mm:ss')}`,
