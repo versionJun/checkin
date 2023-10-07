42aa28618b3c79587dcf52103aafb0b6ce2d48bd
versionJun <55729868+versionJun@users.noreply.github.com>
2023-10-07 16:01:01 +0800

    Update ikuuu_checkin.js

--- task/ikuuu_checkin.js
+++ task/ikuuu_checkin.js
@@ -2,6 +2,12 @@ const axios = require("axios")
 const jsdom = require("jsdom")
 const path = require('path')
 const { sent_message_by_pushplus } = require('../utils/message.js')
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault('Asia/Shanghai')
 
 const BASE_URL = "https://ikuuu.art/";
 
@@ -98,7 +104,7 @@ function go_user_url(cookie){
     }
 
     await sent_message_by_pushplus({ 
-        title: `${path.parse(__filename).name}_${new Date().toLocaleString()}`,
+        title: `${path.parse(__filename).name}_${dayjs.tz().format('YYYY-MM-DD HH:mm:ss')}`,
         message: message.join('\n') 
     });
 
