f5f2502e2339bc18ae6c3f49979a9ac51cce6357
versionJun <55729868+versionJun@users.noreply.github.com>
2023-10-31 22:34:45 +0800

    Create ikuuu_accounts.js

---
+++ config/ikuuu_accounts.js
@@ -0,0 +1,16 @@
+module.exports = [{
+    email: process.env.IKUUU_EMAIL_0,
+    passwd: process.env.IKUUU_PASSWD_0,
+}, {
+    email: process.env.IKUUU_EMAIL_1,
+    passwd: process.env.IKUUU_PASSWD_1,
+}, {
+    email: process.env.IKUUU_EMAIL_2,
+    passwd: process.env.IKUUU_PASSWD_2,
+}, {
+    email: process.env.IKUUU_EMAIL_3,
+    passwd: process.env.IKUUU_PASSWD_3,
+}, {
+    email: process.env.IKUUU_EMAIL_4,
+    passwd: process.env.IKUUU_PASSWD_4,
+}]
