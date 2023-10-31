2dbb2e044102ea729ae66de177ca16bd695e7200
versionJun <55729868+versionJun@users.noreply.github.com>
2023-10-31 23:11:54 +0800

    Create v2free_accounts.js

---
+++ config/v2free_accounts.js
@@ -0,0 +1,16 @@
+module.exports = [{
+    email: process.env.V2FREE_EMAIL_0,
+    passwd: process.env.V2FREE_PASSWD_0,
+}, {
+    email: process.env.V2FREE_EMAIL_1,
+    passwd: process.env.V2FREE_PASSWD_1,
+}, {
+    email: process.env.V2FREE_EMAIL_2,
+    passwd: process.env.V2FREE_PASSWD_2,
+}, {
+    email: process.env.V2FREE_EMAIL_3,
+    passwd: process.env.V2FREE_PASSWD_3,
+}, {
+    email: process.env.V2FREE_EMAIL_4,
+    passwd: process.env.V2FREE_PASSWD_4,
+}]
