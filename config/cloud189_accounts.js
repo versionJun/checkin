e9bfa35e346f0b19efc1774f4c2a744797541299
versionJun <55729868+versionJun@users.noreply.github.com>
2023-11-02 18:40:50 +0800

    Create cloud189_accounts.js

---
+++ config/cloud189_accounts.js
@@ -0,0 +1,16 @@
+module.exports = [{
+    username: process.env.CLOUD189_USERNAME_0,
+    password: process.env.CLOUD189_PASSWORD_0,
+}, {
+    username: process.env.CLOUD189_USERNAME_1,
+    password: process.env.CLOUD189_PASSWORD_1,
+}, {
+    username: process.env.CLOUD189_USERNAME_2,
+    password: process.env.CLOUD189_PASSWORD_2,
+}, {
+    username: process.env.CLOUD189_USERNAME_3,
+    password: process.env.CLOUD189_PASSWORD_3,
+}, {
+    username: process.env.CLOUD189_USERNAME_4,
+    password: process.env.CLOUD189_PASSWORD_4,
+}]
