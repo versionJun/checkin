19b7369d57e56e3a858eeecd6174302c309811c9
versionJun <55729868+versionJun@users.noreply.github.com>
2023-11-02 18:49:58 +0800

    Create cloud189_checkin.js

---
+++ task/cloud189_checkin.js
@@ -0,0 +1,266 @@
+const axios = require('axios')
+const path = require('path')
+const url = require('url') 
+const JSEncrypt = require('node-jsencrypt')
+const { CookieJar } = require('tough-cookie')
+const { wrapper } = require('axios-cookiejar-support')
+wrapper(axios)
+const { sent_message_by_pushplus } = require('../utils/message.js')
+const accounts = require('../config/cloud189_accounts.js')
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault('Asia/Shanghai')
+
+const config = {
+    clientId: '538135150693412',
+    model: 'KB2000',
+    version: '9.0.6',
+    pubKey: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCZLyV4gHNDUGJMZoOcYauxmNEsKrc0TlLeBEVVIIQNzG4WqjimceOj5R9ETwDeeSN3yejAKLGHgx83lyy2wBjvnbfm/nLObyWwQD/09CmpZdxoFYCH6rdDjRpwZOZ2nXSZpgkZXoOBkfNXNxnN74aXtho2dqBynTw3NFTWyQl8BQIDAQAB', 
+}
+
+const config_headers = {
+    'User-Agent': `Mozilla/5.0 (Linux; U; Android 11; ${config.model} Build/RP1A.201005.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/${config.version} Android/30 clientId/${config.clientId} clientModel/${config.model} clientChannelId/qq proVersion/1.0.6`,
+    Referer: 'https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1',
+    'Accept-Encoding': 'gzip, deflate',
+    Host: 'cloud.189.cn',
+}
+
+// 获取公钥 
+const ENCRYPTCONF_URL = 'https://open.e.189.cn/api/logbox/config/encryptConf.do'
+
+// 获取登录参数 lt reqId
+const REDIRECT_URL = 'https://cloud.189.cn/api/portal/loginUrl.action?redirectURL=https://cloud.189.cn/web/redirect.html?returnURL=/main.action'
+
+// 获取登录参数 returnUrl paramId
+const APPCONF_URL = 'https://open.e.189.cn/api/logbox/oauth2/appConf.do'
+
+// 获取登录地址,跳转到登录页
+const LOGINSUBMIT = 'https://open.e.189.cn/api/logbox/oauth2/loginSubmit.do'
+
+// 签到
+const USERSIGN_URL = `https://cloud.189.cn/mkt/userSign.action?rand=${new Date().getTime()}&clientType=TELEANDROID&version=${config.version}&model=${config.model}`
+
+// 天天抽红包
+const DRAWPRIZEMARKETDETAILS_URL = [
+    `https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN`,
+    `https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN_PHOTOS&activityId=ACT_SIGNIN`,
+    `https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_2022_FLDFS_KJ&activityId=ACT_SIGNIN`,
+]
+
+function goEncryptConf(){
+    if (config.pubKey)
+        return config.pubKey
+    return axios(ENCRYPTCONF_URL, {
+        method: 'POST',
+        data: {
+            appId: 'cloud'
+        }
+    })
+    .then(res => {
+        console.log(res.data)
+        if(res.data.result !== 0)
+            return Promise.reject(res.data)
+        return res.data.data.pubKey
+    })
+    .catch(error => {
+        console.error(error)
+        return Promise.reject(`goEncryptConf->${error}`)
+    })
+}
+
+function goRedirect() {
+    return axios(REDIRECT_URL, {
+        method: 'GET'
+    })
+    .then(res => {
+        const urlStr = res.request.res.responseUrl
+        const { query } = url.parse(urlStr, true)
+        return query
+    })
+    .catch(error => {
+        console.error(e)
+        return Promise.reject(`goRedirect->${error}`)
+    })
+
+}
+
+function goAppConf(username, password, encryptKey, query) {
+    return axios(APPCONF_URL, {
+        method: 'POST',
+        data: {
+            version: '2.0',
+            appKey: 'cloud'
+        },
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/76.0',
+            'Referer': 'https://open.e.189.cn/',
+            lt: query.lt,
+            REQID: query.reqId,
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+    })
+    .then(res => {
+        if(res.data.result !== '0')
+            return Promise.reject(`${res.data.msg}`)
+
+        const keyData = `-----BEGIN PUBLIC KEY-----\n${encryptKey}\n-----END PUBLIC KEY-----`;
+        const jsencrypt = new JSEncrypt()
+        jsencrypt.setPublicKey(keyData)
+        const usernameEncrypt = Buffer.from(jsencrypt.encrypt(username), 'base64').toString('hex')
+        const passwordEncrypt = Buffer.from(jsencrypt.encrypt(password), 'base64').toString('hex')
+        const formData = {
+            returnUrl: res.data.data.returnUrl,
+            paramId: res.data.data.paramId,
+            lt: `${query.lt}`,
+            reqId: `${query.reqId}`,
+            userName: `{NRP}${usernameEncrypt}`,
+            password: `{NRP}${passwordEncrypt}`
+        }
+
+        return formData
+    })
+    .catch(error => {
+        console.error(error)
+        return Promise.reject(`goAppConf->${error}`)
+    })    
+}
+
+function goLoginSubmit(formData) {
+    return axios(LOGINSUBMIT, {
+        method: 'POST',
+        data: {
+            appKey: 'cloud',
+            version: '2.0',
+            accountType: '01',
+            mailSuffix: '@189.cn',
+            validateCode: '',
+            returnUrl: formData.returnUrl,
+            paramId: formData.paramId,
+            captchaToken: '',
+            dynamicCheck: 'FALSE',
+            clientType: '1',
+            cb_SaveName: '0',
+            isOauth2: false,
+            userName: formData.userName,
+            password: formData.password,
+        },
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/76.0',
+            'Referer': 'https://open.e.189.cn/',
+            'lt': `${formData.lt}`,
+            'REQID': `${formData.reqId}`,
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+    })
+    .then(res => {
+
+        if(res.data.result !== 0)
+            return Promise.reject(`${res.data.msg}`)
+
+        return res.data.toUrl
+    })
+    .catch(error => {
+        console.error(error)
+        return Promise.reject(`goLoginSubmit->${error}`)
+    })
+}
+
+function goToUrl(toUrl, cookieJar){
+    return axios(toUrl, {
+        method: 'GET',
+        headers: config_headers,
+        jar: cookieJar
+    })
+    .then(res => {
+
+        // console.log(res)
+
+    })
+    .catch(error => {
+        console.error(error)
+        return Promise.reject(`goToUrl->${error}`)
+    })
+
+}
+
+function goUserSign(cookieJar, msg){
+    return axios(USERSIGN_URL, {
+        method: 'GET',
+        jar: cookieJar
+    })
+    .then(res => {
+      
+        msg.push(`${res.data.isSign ? '已经签到' : '签到成功'},获得${res.data.netdiskBonus}M空间`)
+      
+    })
+    .catch(error => {
+        console.error(error)
+        return Promise.reject(`goUserSign->${error}`) 
+    })
+}
+
+async function goDrawPrizeMarketDetails(cookieJar, msg){
+    for (let index = 0; index < DRAWPRIZEMARKETDETAILS_URL.length; index++) {
+        await axios(DRAWPRIZEMARKETDETAILS_URL[index], {
+            method: 'GET',
+            jar: cookieJar
+        })
+        .then(res => {
+            
+            if (res.data.errorCode === 'User_Not_Chance') 
+                msg.push(`第${index+1}次抽奖失败,次数不足`)
+            else
+                msg.push(`第${index+1}次抽奖成功,抽奖获得${res.data.prizeName}`)
+
+        })
+        .catch(error => {
+            console.error(error)
+            return Promise.reject(`goDrawPrizeMarketDetails->${error}`)
+        })
+    }
+}
+
+
+!(async () => {
+
+    const message = []
+    for (let index = 0; index < accounts.length; index++) {
+        const user = accounts[index]
+        if(!user.username || !user.password)
+            continue
+        const msg = [`账号${index}`]
+        try {
+            const encryptKey = await goEncryptConf()
+    
+            const query = await goRedirect()
+    
+            const formData = await goAppConf(user.username, user.password, encryptKey, query)
+    
+            const toUrl = await goLoginSubmit(formData)
+
+            const cookieJar = new CookieJar()
+
+            await goToUrl(toUrl, cookieJar)
+            
+            await goUserSign(cookieJar, msg)
+    
+            await goDrawPrizeMarketDetails(cookieJar, msg)
+        
+        } catch(error) {
+            console.error(error)
+            msg.push(error)
+        } finally {
+            message.push(msg.join('---'))
+        }
+    }
+    
+    // console.log(message)
+
+    await sent_message_by_pushplus({ 
+        title: `${path.parse(__filename).name}_${dayjs.tz().format('YYYY-MM-DD HH:mm:ss')}`,
+        message: message.join('\n') 
+    });
+})()
