cbf804e2e880aafbe89d1a3fa77ee6d5943bf89b
versionJun <55729868+versionJun@users.noreply.github.com>
2025-07-14 11:36:27 +0800

    Update hifiti_accounts.js

--- config/hifiti_accounts.js
+++ config/hifiti_accounts.js
@@ -1,12 +1,9 @@
-module.exports = [
-{
+module.exports = [{
     email: process.env.HIFITI_EMAIL_0 ,
     password: process.env.HIFITI_PASSWORD_0,
     cookie: process.env.HIFITI_COOKIE_0
-}, 
-{
+}, {
     email: process.env.HIFITI_EMAIL_1,
     password: process.env.HIFITI_PASSWORD_1,
     cookie: process.env.HIFITI_COOKIE_1
-}
-]
+}]
